30c37f9c5bf3ae15024e4543e7553dfe0c6455d4
thaibinh <thaibinhdevgit@gmail.com>
2025-05-18 08:39:54 +0700

    fix UI and add screen splash and let start

--- app/src/main/java/com/example/btl_android_studyapp/SplashActivity.java
+++ app/src/main/java/com/example/btl_android_studyapp/SplashActivity.java
@@ -1,6 +1,10 @@
 package com.example.btl_android_studyapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,27 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Context context = getApplicationContext();
+        SharedPreferences sharedPref = context.getSharedPreferences("BTLAndroid", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        boolean daVao = sharedPref.getBoolean("DaVao", false);
+
+        if (daVao) {
+            new Handler().postDelayed(() -> {
+                editor.putBoolean("DaVao", true);
+                editor.apply();
+                startActivity(new Intent(SplashActivity.this, LogInActivity.class));
+                finish();
+            }, 1500); // 1.5 giây
+        } else {
+            new Handler().postDelayed(() -> {
+                editor.putBoolean("DaVao", true);
+                editor.apply();
+                startActivity(new Intent(SplashActivity.this, LetStartActivity.class));
+                finish();
+            }, 1500); // 1.5 giây
+        }
     }
 }
